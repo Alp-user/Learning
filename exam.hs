d36f1326e2980c99215da595225c48b4d2ea7f9c
Alp Arargüç <alpoararguc@gmail.com>
2025-04-22 00:01:08 +0300

    I don't know what to say!

--- exam.hs
+++ exam.hs
@@ -41,3 +41,41 @@ gls = [(x,y)| x<-[1..5], y<-[2,4,6]]
 
 iter f s 0 = s
 iter f s n = f (iter f s (n-1))
+
+y = (1:2:y)
+taken 0 _ = []
+taken n (a:b) = a:(taken (n-1) b)
+
+--infinite factorial list
+fact = 1:1:factlist (tail fact) [2..] where
+    factlist (a:b) (c:d) = (a*c):(factlist b d)
+
+--Classes etc
+
+data Myvalues = A Char| B Bool | C Int
+instance Show Myvalues where -- show function is a->Str!!!Str
+    show (A x) = show x
+    show (B x) = show x
+    show (C x) = show x
+
+instance Eq Myvalues where 
+    val1 == val2 = case val1 of
+        (A x) -> case val2 of
+            (C y) -> True
+            otherwise -> False
+        (B x) -> True
+
+class Array a where
+    top :: (a v) -> v
+    
+instance Array Mylist where
+    top (Cons(a, b)) = a
+
+-- Be fluent with reading functiop type signatures. 
+double x = x*2
+evenn x = x`mod`2 == 0
+g1 = (foldl add 0).(map double) . (filter (evenn))
+-- type signature for(foldl add 0).(map double) . (filter (evenn)) [1,2,3]: ([Int]->Int)->([Int]->[Int])->([Int]->[Int])->[Int]-> Int
+-- As you can see the functions are right associative and (a->b)->(c->a) output of the function to the right should match with the input of the function to the left
+
+iden x = x -- any function like this or where we can use multiple types are actually parameter polymorphism as they apply the same on different types.
